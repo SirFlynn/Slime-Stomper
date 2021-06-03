17761075395a2c0f56741c7a46995daa46323c92
Sir Flynn <flynnvanstryp@gmai.com>
2021-06-03 14:02:40 +1000

    Basic changes to player script and environment

--- Slime Stomper/Assets/Scripts/Player.cs
+++ Slime Stomper/Assets/Scripts/Player.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
+    //creating variables that control the speed the player moves and how heigh they can jump based on the user's input in Unity
+    public float moveSpeed = 5.0f;
+    public float jumpHeight;
+
+    //creates a variable to help check if the player is touching the ground
+    private bool isOnGround;
+
+    //creating variables to help with calculation later
+    [Space]
+    public Rigidbody rb;
+    //public Transform groundChecker;
+    //public LayerMask ground;
+    //Vector3 movement;
+
+
     void Start()
     {
-        
+        //The player is on the ground
+        isOnGround = true;
+
+        //Gets the Rigidbody component from the gameObject and stores it in the variable 'rb'
+        rb = gameObject.GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        //Makes the player move forward  
+        rb.AddForce(new Vector3(moveSpeed, 0, 0));
+
+        //If Space is pressed and the player is touching the ground they will Jump
+        if ((isOnGround == true) && (Input.GetKeyDown(KeyCode.Space)))
+        {
+            rb.AddForce(new Vector3(0, jumpHeight, 0), ForceMode.Impulse);
+        }
     }
 }
